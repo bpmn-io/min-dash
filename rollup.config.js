73416938343ce7c8d8b95b985b817ed675d18fd9
Nico Rehwaldt <git_nikku@nixis.de>
2018-04-01 11:42:50 +0200

    chore(package): add browser field
    + rename esm bundle to dist/index.esm.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,62 +1,52 @@
 import uglify from 'rollup-plugin-uglify';
-
 import babel from 'rollup-plugin-babel';
 
-function bbl() {
-  return babel({
-    exclude: 'node_modules/**',
-    babelrc: false,
-    plugins: [
-      'transform-object-assign'
-    ],
-    presets: [
-      [ 'env', { modules: false } ]
-    ]
-  });
+import pkg from './package.json';
+
+function pgl(plugins=[]) {
+  return [
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      plugins: [
+        'transform-object-assign'
+      ],
+      presets: [
+        [ 'env', { modules: false } ]
+      ]
+    }),
+    ...plugins
+  ];
 }
 
 export default [
+  // browser-friendly UMD build
   {
     input: 'lib/index.js',
     output: {
-      file: 'dist/index.mjs',
-      format: 'es'
+      name: 'MinDash',
+      file: pkg.browser,
+      format: 'umd'
     },
-    plugins: [
-      bbl()
-    ]
-  },
-  {
-    input: 'lib/index.js',
-    output: {
-      file: 'dist/index.js',
-      format: 'cjs'
-    },
-    plugins: [
-      bbl()
-    ]
+    plugins: pgl()
   },
   {
     input: 'lib/index.js',
     output: {
       name: 'MinDash',
-      file: 'dist/min-dash.js',
+      file: pkg.browser.replace(/\.js$/, '.min.js'),
       format: 'umd'
     },
-    plugins: [
-      bbl()
-    ]
-  },
-  {
-    input: 'lib/index.js',
-    output: {
-      name: 'MinDash',
-      file: 'dist/min-dash.min.js',
-      format: 'umd'
-    },
-    plugins: [
-      bbl(),
+    plugins: pgl([
       uglify()
-    ]
+    ])
+  },
+  {
+    input: 'lib/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: pgl()
   }
 ];
